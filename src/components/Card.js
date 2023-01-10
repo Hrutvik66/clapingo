f57bfe581e056cec7d380671017820b970506c3f
Hrutvik66 <hrutvikmalshikare66@gmail.com>
2023-01-10 14:37:51 +0530

    Bug fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ const Card = ({ image, heading, text }) => {
   return (
     <div className="w-[25rem] h-[25rem] rounded-lg">
       {/* Image */}
-      <img src={image} alt={heading} className="relative h-[23rem] w-full  rounded-lg" />
+      <img src={image} alt={heading} className="relative h-[20rem] w-full  rounded-lg" />
       {/* Text */}
       <div className="relative z-40 -top-[7.8rem] text-left bg-[#00000040] backdrop-blur-lg px-10 py-3 h-[8rem]  rounded-b-lg">
         <h4 className="text-[#00D2FF]">{heading}</h4>
